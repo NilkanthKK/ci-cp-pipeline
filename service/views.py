004635efd7021af71521bb2bd40034d64c1662b5
NilkanthKK <nilkanth.koffeekodes@gmail.com>
2025-02-27 12:33:31 +0530

    done

--- service/views.py
+++ service/views.py
@@ -4,6 +4,13 @@ from .models import *
 from .serializers import *
 from rest_framework.decorators import api_view
 
+from django.core.management.utils import get_random_secret_key
+
+secret_key = get_random_secret_key()
+print('====================',secret_key)
+
+
+
 # Create your views here.
 @api_view(['GET'])
 def ProductListCreate(request):
